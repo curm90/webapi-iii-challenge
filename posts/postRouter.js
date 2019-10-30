ef18fb0df734765584e997bf95fe9cf10674da06
Liam Sutton <devliam90@gmail.com>
2019-10-30 16:41:32 +0000

    add posts endpoints (stretch)

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,17 +1,82 @@
-const express = 'express';
+const express = require('express');
+const posts = require('./postDb');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+// Get all posts
+router.get('/', (req, res) => {
+  posts
+    .get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'Something went horribly wrong! ' + err.message });
+    });
+});
 
-router.get('/:id', (req, res) => {});
+// Get a specific post
+router.get('/:id', validatePostId, (req, res) => {
+  res.status(200).json(req.post);
+});
 
-router.delete('/:id', (req, res) => {});
+// Delete a post
+router.delete('/:id', validatePostId, (req, res) => {
+  posts
+    .remove(req.params.id)
+    .then(post => {
+      res.status(200).json({ message: 'Post deleted succesfully' });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Your request could not be excecuted! ' + err.message
+      });
+    });
+});
 
-router.put('/:id', (req, res) => {});
+// Update a post
+router.put('/:id', [validatePostId, validatePost], (req, res) => {
+  posts
+    .update(req.params.id, req.body)
+    .then(postToUpdate => {
+      res.status(201).json(postToUpdate);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Your request could not be processed! ' + err.message
+      });
+    });
+});
 
-// custom middleware
+// Custom middleware
+function validatePostId(req, res, next) {
+  posts
+    .getById(req.params.id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        res.status(400).json({ message: 'This post does not exist' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Your request could not be processed. ' + err.message
+      });
+    });
+}
 
-function validatePostId(req, res, next) {}
+function validatePost(req, res, next) {
+  if (Object.keys(req.body).length && req.body.text) {
+    next();
+  } else if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: 'missing post data' });
+  } else {
+    res.status(400).json({ message: 'missing required text field' });
+  }
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const helmet = require('helmet');
 const server = express();
 
 const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 
 server.use(helmet());
+server.use(logger);
 server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use('/api/users', logger, userRouter);
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 
 //custom middleware
 function logger(req, res, next) {
